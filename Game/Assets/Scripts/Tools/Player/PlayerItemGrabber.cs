203cae694dc39b6ef7e734871b1d35ed2594eae6
Juutis <jutkula@gmail.com>
2020-10-03 12:35:50 +0300

    Added sword item.

---
+++ Game/Assets/Scripts/Tools/Player/PlayerItemGrabber.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemGrabber : MonoBehaviour
+{
+    private PlayerStrike playerStrike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStrike = GetComponent<PlayerStrike>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Grab(Item item)
+    {
+        Debug.Log("Picked up " + item.ItemType);
+
+        switch(item.ItemType)
+        {
+            case ItemType.BASIC_SWORD:
+                playerStrike.EnableWeapon();
+                break;
+        }
+
+    }
+}
--- Game/Assets/Scripts/Tools/Player/PlayerStrike.cs
+++ Game/Assets/Scripts/Tools/Player/PlayerStrike.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlayerStrike : MonoBehaviour
 {
+    [SerializeField]
+    GameObject PlayerWeapon;
+
+    [SerializeField]
+    bool WeaponEnabled;
+
     CharacterAnimator charAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         charAnim = GetComponentInChildren<CharacterAnimator>();
+        PlayerWeapon.SetActive(WeaponEnabled);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (WeaponEnabled && Input.GetKey(KeyCode.Mouse0))
         {
             charAnim.Strike();
         }
     }
+
+    public void EnableWeapon()
+    {
+        WeaponEnabled = true;
+        PlayerWeapon.SetActive(WeaponEnabled);
+    }
+
 }
