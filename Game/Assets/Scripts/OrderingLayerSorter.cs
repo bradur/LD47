c73e8842b1a7d3cd781fb229adb6cc86395c3797
Juutis <jutkula@gmail.com>
2020-10-03 13:51:39 +0300

    Added ordering layer stuffs

---
+++ Game/Assets/Scripts/OrderingLayerSorter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderingLayerSorter : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sortingOrder = (int)(-transform.position.y * 100);
+    }
+}
