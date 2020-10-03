8f9b11440b367fa466f682d660169ec4e131ce23
Matias Pitkänen <M2tias@Gmail.com>
2020-10-03 16:35:17 +0300

    Game loops after non-skill resource runs out. Doesn't reset skill-resources.

--- Game/Assets/Scripts/LoopManagement/LoopManager.cs
+++ Game/Assets/Scripts/LoopManagement/LoopManager.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoopManager : MonoBehaviour
 {
     public static LoopManager main;
-    
-    void Awake() {
+
+    void Awake()
+    {
+        this.transform.parent = null;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("LoopManager");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
         main = this;
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    void Start () {
+    void Start()
+    {
         HUDManager.main.Init();
         PlayerResources.main.Init();
         Reset();
     }
 
-    public void Reset() {
+    public void Reset(bool reload = false)
+    {
         PlayerResources.main.Reset();
         HUDManager.main.Refresh();
+
+        if (reload)
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
     }
 }
