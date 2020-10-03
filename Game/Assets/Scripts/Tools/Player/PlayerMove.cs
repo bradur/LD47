6081ab1fb6b268f71812d595946a507f95bcb102
Juho Saarelainen <bradurdung@gmail.com>
2020-10-03 08:29:08 +0300

    Movement proto & placeholder / UI art.

---
+++ Game/Assets/Scripts/Tools/Player/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerRotate))]
+public class PlayerMove : MonoBehaviour
+{
+    private Vector2 moveInput;
+
+    [SerializeField]
+    private float moveSpeed = 3.0f;
+    [SerializeField]
+    private float moveModifier = 1.0f;
+    private Rigidbody playerRigidbody;
+    private PlayerRotate playerRotate;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerRotate = GetComponent<PlayerRotate>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var mousePos3d = Input.mousePosition;
+        mousePos3d.z = Mathf.Abs(Camera.main.transform.position.z);
+        var mousePos = Camera.main.ScreenToWorldPoint(mousePos3d);
+        playerRotate.SetTarget(mousePos);
+    }
+
+    private void FixedUpdate()
+    {
+        var velocityDir = moveInput.magnitude > 1.0f ? moveInput.normalized : moveInput;
+        playerRigidbody.velocity = velocityDir * moveSpeed * moveModifier;
+    }
+}
---
+++ Game/Assets/Scripts/Tools/Player/PlayerRotate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    private GameObject sprite;
+    Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var rend in GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (rend.name == "Character Sprite")
+            {
+                sprite = rend.gameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetDir = target - MyPos();
+        var angleDiff = Vector2.SignedAngle(sprite.transform.up, targetDir);
+        sprite.transform.Rotate(Vector3.forward, angleDiff);
+    }
+
+    public void SetTarget(Vector2 newTarget)
+    {
+        target = newTarget;
+    }
+
+    private Vector2 MyPos()
+    {
+        return transform.position;
+    }
+}
