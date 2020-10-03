ce7a65633c7178118a75f27e8da770e72e2bf4fc
Juutis <jutkula@gmail.com>
2020-10-03 12:00:59 +0300

    Added damage mechanics.

---
+++ Game/Assets/Scripts/Character/CollisionDamager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamager : MonoBehaviour
+{
+    [SerializeField]
+    public float Damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageable = other.GetComponent<Damageable>();
+        if (damageable != null)
+        {
+            damageable.Hurt(Damage);
+        }
+    }
+}
