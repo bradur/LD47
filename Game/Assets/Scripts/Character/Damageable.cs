eea803b26be1dfede6b7aaefbb86ccb875f2bc81
Juutis <jutkula@gmail.com>
2020-10-03 17:46:22 +0300

    Added shake effect for damage.

--- Game/Assets/Scripts/Character/Damageable.cs
+++ Game/Assets/Scripts/Character/Damageable.cs
@@ -13,11 +13,21 @@ public class Damageable : MonoBehaviour
     [SerializeField]
     float Health = 30.0f;
 
+    [SerializeField]
+    float HurtShakeX = 1.0f;
+
+    [SerializeField]
+    float HurtShakeY = 0.0f;
+
     SpriteRenderer[] renderers;
     Color[] originalColors;
 
     float hurtTimer;
     public static readonly float HURT_DURATION = 0.5f;
+    public static readonly float HURT_SHAKE_MAGNITUDE_X = 0.1f;
+    public static readonly float HURT_SHAKE_MAGNITUDE_Y = 0.1f;
+    Vector3 hurtPosition;
+    bool positionsReset = true;
 
     // Start is called before the first frame update
     void Start()
@@ -37,15 +47,32 @@ public class Damageable : MonoBehaviour
         {
             float amount = (hurtTimer - Time.time) / HURT_DURATION;
             tint(HurtTint, amount);
+            if (HurtShakeX > 0.01f || HurtShakeY > 0.01f)
+            {
+                var shakeX = HurtShakeX * HURT_SHAKE_MAGNITUDE_X * Mathf.Sin(amount * Mathf.PI * 10);
+                var shakeY = HurtShakeY * HURT_SHAKE_MAGNITUDE_Y * Mathf.Sin(amount * Mathf.PI * 10);
+                transform.position = hurtPosition + new Vector3(shakeX, shakeY, 0.0f);
+            }
         }
         else
         {
             tint(HurtTint, 0.0f);
+            if (!positionsReset)
+            {
+                positionsReset = true;
+                transform.position = hurtPosition;
+            }
         }
     }
 
     public void Hurt(float damage)
     {
+        if (hurtTimer < Time.time)
+        {
+            hurtPosition = transform.position;
+            positionsReset = false;
+        }
+
         Health -= damage;
         if (Health > 0)
         {
