db86d7f8ef3d1df3128cfc4c26cdcf7e570ec879
Juutis <jutkula@gmail.com>
2020-10-04 18:46:53 +0300

    Lake prefab

---
+++ Game/Assets/Scripts/OrderingLayerRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderingLayerRandomizer : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.sortingOrder = Random.Range(int.MinValue, int.MaxValue);
+    }
+}
